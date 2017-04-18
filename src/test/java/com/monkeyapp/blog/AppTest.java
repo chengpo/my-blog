061030b14889694af64ba429713c8520ddedaf19
po_cheng <chainpaul@gmail.com>
2017-04-18 10:42:01 -0700

    update js lib resource folder hierarchy

--- src/test/java/com/monkeyapp/blog/AppTest.java
+++ src/test/java/com/monkeyapp/blog/AppTest.java
@@ -1,36 +1,29 @@
 package com.monkeyapp.blog;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+@RunWith(JUnit4.class)
+public class AppTest
 {
     /**
      * Create the test case
-     *
-     * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public AppTest( )
     {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
     }
 
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp()
     {
         assertTrue( true );
