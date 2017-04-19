d70aecef52d8c021b5bb73822ca4c3aaf2de10fa
po_cheng <chainpaul@gmail.com>
2017-04-19 15:53:03 -0700

    populate PostBank module class

--- src/main/java/com/monkeyapp/blog/rest/StatusResource.java
+++ src/main/java/com/monkeyapp/blog/rest/StatusResource.java
@@ -1,49 +1,18 @@
 package com.monkeyapp.blog.rest;
 
+import com.monkeyapp.blog.rest.module.PostBank;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Optional;
 
 @Path("/status")
 public class StatusResource {
     @GET
     public Response get(@Context HttpHeaders headers) {
-            Optional<URL> fileListUrl = Optional.ofNullable(Thread.currentThread()
-                                                .getContextClassLoader()
-                                                .getResource("posts/file-list.json"));
-
-            return fileListUrl.flatMap(StatusResource::dumpPostList)
-                              .orElseGet(StatusResource::errorResponse);
-    }
-
-    private static Optional<Response> dumpPostList(URL url) {
-       try {
-            return Optional.of(
-                    Response.ok(new String(
-                                        Files.readAllBytes(Paths.get(url.toURI())),
-                                        StandardCharsets.UTF_8),
-                                MediaType.APPLICATION_JSON)
-                            .build());
-
-        } catch (IOException | URISyntaxException e) {
-            return Optional.empty();
-        }
-    }
-
-    private static Response errorResponse() {
-        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                .entity("Post list not found")
-                .type(MediaType.TEXT_PLAIN)
-                .build();
+        return Response.ok(new PostBank().toString(), MediaType.TEXT_PLAIN).build();
     }
 }
---
+++ src/main/java/com/monkeyapp/blog/rest/module/PostBank.java
@@ -0,0 +1,52 @@
+package com.monkeyapp.blog.rest.module;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+public class PostBank {
+    private final List<String> mPostNameList;
+
+    public PostBank() {
+        final Optional<InputStream> optionalUrl = Optional.ofNullable(
+                                    Thread.currentThread()
+                                        .getContextClassLoader()
+                                        .getResourceAsStream("posts/file-list.json"));
+
+        mPostNameList = optionalUrl.flatMap(PostBank::fromJson)
+                               .orElseGet(Collections::emptyList);
+    }
+
+    public List<String> getAllPosts() {
+        return mPostNameList;
+    }
+
+    private static Optional<List<String>> fromJson(InputStream input) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(input))) {
+            return Optional.of(new Gson().fromJson(reader, new TypeToken<ArrayList<String>>(){}.getType()));
+        } catch (IOException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("PostBank --- ");
+        sb.append(String.valueOf(mPostNameList.size())).append(" posts in total:\n");
+        sb.append("[\n");
+
+        mPostNameList.forEach(post -> sb.append(post).append("\n"));
+
+        sb.append("]\n");
+        return sb.toString();
+    }
+}
