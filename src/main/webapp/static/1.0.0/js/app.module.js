9769a3b1879804d12bf48328a03acc618418e654
po_cheng <chainpaul@gmail.com>
2017-05-18 12:17:45 -0700

    populate ngprogress

--- src/main/webapp/static/1.0.0/js/app.module.js
+++ src/main/webapp/static/1.0.0/js/app.module.js
@@ -2,6 +2,7 @@
 
 angular.module('blogApp', [
             'ngAnimate',
+            'ngProgress',
             'core',
             'sidebar',
             'about',
@@ -9,4 +10,17 @@ angular.module('blogApp', [
             'postDetail'])
         .run(['$animate', function($animate) {
             $animate.enabled(true);
-          }]);
+          }])
+        .run(function ($rootScope, ngProgressFactory) {
+            // first create instance when app starts
+            $rootScope.progressbar = ngProgressFactory.createInstance();
+            $rootScope.progressbar.setColor('#808080');
+
+            $rootScope.$on("$routeChangeStart", function () {
+                $rootScope.progressbar.start();
+            });
+
+            $rootScope.$on("$routeChangeSuccess", function () {
+                $rootScope.progressbar.complete();
+            });
+        });
