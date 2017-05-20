5891660a0839f59bce0bd7d303ba40ff3880a0f0
po_cheng <chainpaul@gmail.com>
2017-05-20 14:18:17 -0700

    handle network error while loading post content. add horizontal line in post list

--- src/main/webapp/static/1.0.0/js/post-detail/post-detail.component.js
+++ src/main/webapp/static/1.0.0/js/post-detail/post-detail.component.js
@@ -6,11 +6,14 @@ angular.module('postDetail')
             controller: ['$routeParams', 'postContent',
                 function postDetailController($routeParams, postContent) {
                     var self = this;
+                    self.error = "";
+
                     postContent.get({name:$routeParams.name},
                                      function(detail) {
                                         self.entity = detail.entity;
                                         self.content = detail.content;
+                                    }, function(error) {
+                                        self.error = "Failed to retrieve post content, status: " + error.status + "!";
                                     });
-                }
-            ]
+                }]
        });
