bbcaa48a8290c51ddae683e231fedbbc53450f77
po_cheng <chainpaul@gmail.com>
2017-06-29 00:52:45 +0800

    use html5 url style

--- src/main/webapp/static/1.0.0/js/app.config.js
+++ src/main/webapp/static/1.0.0/js/app.config.js
@@ -4,14 +4,14 @@ angular.
   module('blogApp').
   config(['$locationProvider', '$routeProvider',
     function config($locationProvider, $routeProvider) {
-      $locationProvider.hashPrefix('!');
-
+      //$locationProvider.hashPrefix('!');
+      $locationProvider.html5Mode(true);
       $routeProvider.
         when('/posts', {
-          template: '<post-list></post-list>'
+            template: '<post-list></post-list>'
         }).
         when('/posts/:name', {
-          template: '<post-detail></post-detail>'
+            template: '<post-detail></post-detail>'
         }).
         when('/about', {
             template: '<about></about>'
