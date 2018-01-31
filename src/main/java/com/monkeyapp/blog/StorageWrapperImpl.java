8a7769293638770282fc3537d845fd179febae0b
po_cheng <904830+chengpo@users.noreply.github.com>
2018-01-31 00:41:36 -0800

    lazy initialize page / post list

--- src/main/java/com/monkeyapp/blog/StorageWrapperImpl.java
+++ src/main/java/com/monkeyapp/blog/StorageWrapperImpl.java
@@ -27,6 +27,8 @@ package com.monkeyapp.blog;
 import com.monkeyapp.blog.readers.FileListReader;
 import com.monkeyapp.blog.wrappers.FileWrapper;
 import com.monkeyapp.blog.wrappers.StorageWrapper;
+import org.apache.commons.lang3.concurrent.ConcurrentException;
+import org.apache.commons.lang3.concurrent.LazyInitializer;
 
 import java.util.Collections;
 import java.util.List;
@@ -34,16 +36,38 @@ import java.util.function.Function;
 import java.util.function.UnaryOperator;
 
 public class StorageWrapperImpl implements StorageWrapper {
+    private final LazyInitializer<List<String>> postFiles = new LazyInitializer<List<String>>() {
+        @Override
+        protected List<String> initialize() {
+            return loadFileList(AppContext::realPostPath)
+                    .apply("file-list.json");
+        }
+    };
+
+    private final LazyInitializer<List<String>> pageFiles = new LazyInitializer<List<String>>() {
+        @Override
+        protected List<String> initialize() {
+            return loadFileList(AppContext::realPagePath)
+                    .apply("file-list.json");
+        }
+    };
+
     @Override
     public List<String> listPostFiles() {
-        return loadFileList(AppContext::realPostPath)
-                .apply("file-list.json");
+        try {
+            return postFiles.get();
+        } catch (ConcurrentException e) {
+            throw new RuntimeException("Failed to load post file list!", e);
+        }
     }
 
     @Override
     public List<String> listPageFiles() {
-        return loadFileList(AppContext::realPagePath)
-                .apply("file-list.json");
+        try {
+            return pageFiles.get();
+        } catch (ConcurrentException e) {
+            throw new RuntimeException("Failed to load page file list!", e);
+        }
     }
 
     private Function<String, List<String>> loadFileList(UnaryOperator<String> pathTransform) {
