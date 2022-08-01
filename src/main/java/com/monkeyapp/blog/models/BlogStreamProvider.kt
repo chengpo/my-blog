6e16088ba8fe8e903c8262b5e397768ee9fca3c6
Po Cheng <904830+chengpo@users.noreply.github.com>
2022-07-31 20:07:13 -0700

    Update BlogStreamProvider.kt

--- src/main/java/com/monkeyapp/blog/models/BlogStreamProvider.kt
+++ src/main/java/com/monkeyapp/blog/models/BlogStreamProvider.kt
@@ -13,25 +13,27 @@ class BlogStreamProvider(private val root: String,
                          private val context: ServletContext,
                          private val inputStreamProvider: InputStreamProvider,
                          private val fileListJson: String = "file-list.json") {
+  
+    private val blogMetadataFactory = BlogMetadataFactory()
 
     fun metaStream(): Stream<BlogMetadata> {
-        val blogMetadataFactory = BlogMetadataFactory()
-        val fileListJsonPath = pathOf(root, fileListJson)
-        val inputStream = inputStreamProvider.streamOf(fileListJsonPath)
-        val jsonString = BufferedReader(InputStreamReader(inputStream)).use {
-            it.lines().collect(Collectors.joining(System.lineSeparator()))
-        }
-
-        return ObjectMapper()
-            .readValue(
-                jsonString,
-                object : TypeReference<List<String>>() {})
-            .map { name ->
-                pathOf(root, name) to name
-            }
-            .mapNotNull { (path, name) ->
-                blogMetadataFactory.create(path, name)
-            }.stream()
+        return pathOf(root, fileListJson)
+                   .run(inputStreamProvider::streamOf)
+                   .run(::InputStreamReader)
+                   .run(::BufferedReader)
+                   .use { it.lines().collect(Collectors.joining(System.lineSeparator())) }
+                   .run {
+                      ObjectMapper()
+                        .readValue(
+                            this,
+                            object : TypeReference<List<String>>() {})
+                        .map { name ->
+                            pathOf(root, name) to name
+                        }
+                        .mapNotNull { (path, name) ->
+                            blogMetadataFactory.create(path, name)
+                        }.stream()
+                   }
     }
 
     private fun pathOf(root: String, file: String): String {
